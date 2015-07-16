d37227a6e099016e2797fbed2b6e3dbf71667b13
rohitgdeveloper <rohitg.developer@gmail.com>
2015-07-17 00:53:13 +0530

    ghjjjj

---
+++ src/com/example/exampleproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.exampleproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Button b=(Button) findViewById(R.id.button1);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View arg0) {
+		Intent intent=new Intent();
+		//startActivityForResult(intent, requestCode);
+	}
+
+}
